b6c944e055b457265be8eb20223dbdca0119b5d0
haze355 <haze355@gmail.com>
2020-07-16 23:58:11 -0700

    background image setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,32 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { FaCartArrowDown } from "react-icons/fa"
+import BackgroundSection from "../components/Globals/BackgroundSection"
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your ShopItFresh site.</p>
-    <FaCartArrowDown />
+    <BackgroundSection
+      img={data.img.childImageSharp.fluid}
+      title="Shop It Fresh"
+      styleClass="default-background"
+    />
   </Layout>
 )
+
+export const query = graphql`
+  {
+    img: file(relativePath: { eq: "default-background.jpg" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
