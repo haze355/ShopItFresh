d4ab8550b441f1476f0c9d8ed9a275726b3bab18
Bo-Zhang-819 <56896198+Bo-Zhang-819@users.noreply.github.com>
2020-07-30 23:45:37 -0700

    the function for "Contact Us"

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,7 @@ import BackgroundSection from "../components/Globals/BackgroundSection"
 import Info from "../components/Home/Info"
 import Menu from "../components/Home/Menu"
 import Giftsets from "../components/Home/Giftsets"
+import Contact from "../components/Home/Contact";
 
 const IndexPage = ({ data }) => (
   <Layout>
@@ -16,8 +17,10 @@ const IndexPage = ({ data }) => (
       title="Shop It Fresh"
       styleClass="default-background"
     />
+    <Info />
     <Menu items={data.menu} />
     <Giftsets />
+    <Contact />
   </Layout>
 )
 
